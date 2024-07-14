5d88ec55ac78d3e59ad333220db93a9c40945355
Ricki Angel <rickihippy@hotmail.com>
2024-07-14 13:23:36 +0100

    Initial commit with project structure

---
+++ JFrame Exercise/src/Main.java
@@ -0,0 +1,13 @@
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame ("Hello, I'm Ricki");
+        frame.setSize(600,400);
+        frame.setVisible(true);
+
+
+    }
+}
